4f46b7c66b27ab226c8d551b675ba3fd0ddc3744
WEX_279 <legazyuri279@gmail.com>
2026-05-21 13:23:49 +0200

    feat: fix app.js styles

---
+++ app.js
@@ -0,0 +1,21 @@
+const input = document.getElementById("input-tarea")
+const btn = document.getElementById("btn-añadir")
+const list = document.getElementById("lista-tareas")
+
+btn.addEventListener("click", (btn) => {
+    const li = document.createElement("li"); 
+    list.appendChild(li);
+
+    const span = document.createElement("span");
+    li.appendChild(span)
+    span.textContent = input.value;
+    
+
+    const button = document.createElement("button");
+    li.appendChild(button)
+    button.textContent = "❌";
+    button.classList.add("eliminar");
+    button.addEventListener("click", () => li.remove());  
+    input.value = '';  
+});
+
